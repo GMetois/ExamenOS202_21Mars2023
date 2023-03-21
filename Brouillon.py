2c844afe1a3a04c095e6c29f5d6cf803e8bfcb21
Gabriel METOIS <gabriel.metois@ensta-paris.fr>
2023-03-21 09:52:40 +0100

    Fin exo 1

---
+++ Brouillon.py
@@ -0,0 +1,7 @@
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+import sys
+from mpi4py import MPI
+
+4*cells[iter-1, 0:-2]+ 2*cells[iter-1, 1:-1]+ cells[iter-1, 2:]
--- automate_cellulaire_1d.py
+++ automate_cellulaire_1d.py
@@ -9,6 +9,8 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
+print("Hello from rank", rank)
+
 nombre_cas   : int = 256
 nb_cellules  : int = 360  # Cellules fantomes
 nb_iterations: int = 360
@@ -16,11 +18,11 @@ nb_iterations: int = 360
 compute_time = 0.
 display_time = 0.
 
-def save_as_md(cells, symbols='⬜⬛'):
+def save_as_md(cells, rank, symbols='⬜⬛',):
     res = np.empty(shape=cells.shape, dtype='<U')
     res[cells==0] = symbols[0]
     res[cells==1] = symbols[1]
-    np.savetxt(f'resultat_{num_config:03d}.md', res, fmt='%s', delimiter='', header=f'Config #{num_config}', encoding='utf-8')
+    np.savetxt(f'resultat_{num_config:03d}_rank_{rank}.md', res, fmt='%s', delimiter='', header=f'Config #{num_config}', encoding='utf-8')
 
 def save_as_png(cells):
     fig = plt.figure(figsize=(nb_iterations/10., nb_cellules/10.))
@@ -30,8 +32,8 @@ def save_as_png(cells):
     plt.savefig(f"resultat_{num_config:03d}.png", dpi=100, bbox_inches='tight')
     plt.close()
 
-
-for num_config in range(nombre_cas):
+#Parallélisation statique sur la boucle des configurations
+for num_config in range(rank, nombre_cas, size):
     t1 = time.time()
     cells = np.zeros((nb_iterations, nb_cellules+2), dtype=np.int16)
     cells[0, (nb_cellules+2)//2] = 1
@@ -44,7 +46,7 @@ for num_config in range(nombre_cas):
     compute_time += t2 - t1
 
     t1 = time.time()
-    save_as_md(cells)
+    save_as_md(cells, rank)
 #    save_as_png(cells)
     t2 = time.time()
     display_time += t2 - t1
